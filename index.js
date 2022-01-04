546ef0e92595f8c86c106023f0067ebba79e7ff2
Armando Rios <armandorg95@hotmail.com>
2022-01-04 13:49:22 -0600

    fist commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+const db = require("./lib/db");
+const apiRouter = require("./Routes");
+
+
+app.use(express.json())
+
+apiRouter(app);
+
+app.listen(port, () => {
+
+    console.log("Listening on port:", port);
+
+    db.connect().then(() => {
+        console.log("DB Connect");
+    }).catch((err) => {
+        console.error("Connection error", err);
+    });
+});
